f7874416f1c9ebf4b96e64638999d9b24c548ba5
forbesmiyasato <miya5288@pacificu.edu>
2020-11-25 22:51:38 -0800

    added github login with firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './firebaseConfig';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+var provider = new firebase.auth.GithubAuthProvider();
+provider.addScope('repo');
+
+const App = ({user, signOut, signInWithGithub}) => {
+
+    console.log(user);
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGithub}>Sign in with Github</button>
+          }
+        </header>
+      </div>
+    );
+  
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  githubProvider: new firebase.auth.GithubAuthProvider(),
+  scope: 'repo',
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
